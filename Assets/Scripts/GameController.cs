5320e3c12798e3e313469e3dd29a54af81c73504
Unknown <darkilidas@abv.bg>
2018-03-11 02:13:31 +0200

    Score points && GameOver moment

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
@@ -10,17 +11,44 @@ public class GameController : MonoBehaviour {
     public List<GameObject> hazards = new List<GameObject>();
     public float spawnWait;
     public float startWait;
+    public  TextMesh scoreText;
+    public TextMesh restartText;
+    public TextMesh gameOverText;
+
+    private bool gameOver;
+    private bool restart;
     public float waveWait;
+    
+    private int score;
     
 
     private void Start()
     {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        scoreUpdate();
         StartCoroutine(SpawnWWaves());
     }
 
     private void Update()
     {
-      
+      if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
     }
 
     IEnumerator SpawnWWaves()
@@ -37,11 +65,28 @@ public class GameController : MonoBehaviour {
                 yield return new WaitForSeconds(spawnWait);
             }
             yield return new WaitForSeconds(waveWait);
-        }
-       
-        
-        
 
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' to restart";
+                restart = true;
+                break;
+            }
+        }
 
     }
+   
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        scoreUpdate();
+    }
+
+    void scoreUpdate()
+    {
+       scoreText.text = "Score: " + score;
+    }
+
+   
 }
